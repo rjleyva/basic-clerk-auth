f2d42aa0f405e84cac4a73d9a66f44300f4a19bb
RJ <rleyva@tutanota.com>
2025-06-30 09:35:58 -0400

    refactor(auth): improve semantics and accessibility

--- src/components/AuthPage.tsx
+++ src/components/AuthPage.tsx
@@ -5,17 +5,44 @@ import {
   UserButton
 } from '@clerk/clerk-react';
 
+const afterSignOutUrl = '/';
+
 const AuthPage = () => {
   return (
-    <header id="auth-heading">
-      <section id="auth-section">
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </section>
+    <header
+      id="auth-header"
+      className="flex flex-col items-center m-auto p-4 max-w-xl"
+    >
+      <h1
+        id="auth-heading"
+        className="text-xl md:text-2xl lg:text-4xl font-bold my-4"
+      >
+        Welcome to Me in Bytes
+      </h1>
+
+      <p id="auth-message" className="text-base mb-6 font-medium">
+        Sign up for free and start earning rewards.
+      </p>
+
+      <div className="mockup-browser border-base-300 border w-full">
+        <div className="mockup-browser-toolbar">
+          <div className="input">https://meinbytes.com/signin</div>
+        </div>
+
+        <div className="grid place-content-center border-t border-base-300 h-80">
+          <section id="auth-section" aria-labelledby="auth-heading">
+            <SignedOut>
+              <SignInButton mode="modal">
+                <span className="btn btn-primary">Sign In</span>
+              </SignInButton>
+            </SignedOut>
+
+            <SignedIn>
+              <UserButton afterSignOutUrl={afterSignOutUrl} />
+            </SignedIn>
+          </section>
+        </div>
+      </div>
     </header>
   );
 };
