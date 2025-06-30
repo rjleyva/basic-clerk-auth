41961818a7145f3b1b1d30e822441a7d55f62f0d
RJ <rleyva@tutanota.com>
2025-06-30 08:48:25 -0400

    refactor(steps): refactor: improve semantics and accessibility

---
+++ src/components/Steps.tsx
@@ -0,0 +1,32 @@
+const processTitle: string = 'Just 4 Easy Steps to Get You Started';
+
+const Steps = () => {
+  return (
+    <section aria-labelledby="steps-heading" className="py-8">
+      <div className="max-w-xl mx-auto p-4 items-center">
+        <h2
+          id="steps-heading"
+          className="text-lg text-center font-semibold mb-4"
+        >
+          {processTitle}
+        </h2>
+
+        <ol className="steps list-decimal list-inside">
+          <li className="step step-primary" aria-current="step">
+            Register
+          </li>
+          <li className="step step-primary">Choose plan</li>
+          <li className="step">Purchase</li>
+          <li className="step">Receive Product</li>
+        </ol>
+
+        <p className="text-center mx-auto my-4 font-medium">
+          Powered by <span className="sr-only">authentication provider</span>{' '}
+          Clerk
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Steps;
