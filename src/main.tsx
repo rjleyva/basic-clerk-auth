39c6a069129090f677d212dcc47d524bb7f3db8b
RJ <rleyva@tutanota.com>
2025-06-30 03:37:03 -0400

    refactor(main): extract routing logic to routes.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './globals.css';
 import { ClerkProvider } from '@clerk/clerk-react';
-import App from './app.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import routes from './routes.tsx';
+
+const router = createBrowserRouter(routes);
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -13,7 +16,7 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <App />
+      <RouterProvider router={router} />
     </ClerkProvider>
   </StrictMode>
 );
