62d1ac3e4ced6bca4d48e048749e786b8f4b2974
RJ <rleyva@tutanota.com>
2025-06-30 03:56:32 -0400

    feat(nav): creat a basic NavBar with props

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router';
+
+const home: string = 'Home';
+
+const NavBar = () => {
+  return (
+    <header className="top-0 sticky z-10">
+      <nav className="flex items-center justify-between max-w-xl mx-auto p-4 h-26">
+        <Link to="/">{home}</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
