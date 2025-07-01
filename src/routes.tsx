a9f7bea4b22273aa47ff6a6ffcfc6f9a3565b8e7
RJ <rleyva@tutanota.com>
2025-07-01 16:35:08 -0400

    feat(routes): add lazy loading to main components with Suspense fallback

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,21 +1,26 @@
-import App from './app.tsx';
-import Footer from './components/Footer.tsx';
-import NavBar from './components/NavBar.tsx';
+import { lazy, Suspense } from 'react';
+import LoadingRing from './components/LoadingRing.tsx';
+
+const App = lazy(() => import('./app.tsx'));
+const NavBar = lazy(() => import('./components/NavBar.tsx'));
+const Footer = lazy(() => import('./components/Footer.tsx'));
 
 const routes = [
   {
     path: '/',
     element: (
-      <>
-        <NavBar home="The Kapital" />
-        <App />
-        <Footer
-          footerScreenReaderText="Secured by Clerk, an authentication provider"
-          footerMessagePrefix="Secured by"
-          footerLinkLabel="Clerk"
-          footerLinkHref="https://clerk.com/"
-        />
-      </>
+      <Suspense fallback={<LoadingRing />}>
+        <>
+          <NavBar home="The Kapital" />
+          <App />
+          <Footer
+            footerScreenReaderText="Secured by Clerk, an authentication provider"
+            footerMessagePrefix="Secured by"
+            footerLinkLabel="Clerk"
+            footerLinkHref="https://clerk.com/"
+          />
+        </>
+      </Suspense>
     )
   }
 ];
