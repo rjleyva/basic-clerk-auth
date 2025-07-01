54b5cf5d5ff3594a1253719432ff1cc9ca20a2c3
RJ <rleyva@tutanota.com>
2025-07-01 11:32:17 -0400

    refactor(navbar): remove hardcoded values and use props instead

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,17 @@
+import type { JSX } from 'react';
 import { Link } from 'react-router';
 
-const home: string = 'Logo';
+interface NavBarProps {
+  home: string;
+}
 
-const NavBar = () => {
+const NavBar = ({ home }: NavBarProps): JSX.Element => {
   return (
     <header className="top-0 sticky z-10">
       <nav className="flex items-center justify-between max-w-xl mx-auto p-4 h-26">
-        <Link to="/">{home}</Link>
+        <Link to="/" aria-label="Go to homepage" className="font-bold">
+          {home}
+        </Link>
       </nav>
     </header>
   );
